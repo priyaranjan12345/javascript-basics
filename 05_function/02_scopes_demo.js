2c1bc60aec3e19910bdd0623a8126f5fac54d613
Priyaranjan <priyaranjanmantri@gmail.com>
2023-12-04 18:42:01 +0530

    ✅ scope

--- 05_function/02_scopes_demo.js
+++ 05_function/02_scopes_demo.js
@@ -3,6 +3,7 @@ let b = 20
 const c = 30
 
 if(true) {
+    // block scope
     var a = 100 // or same as 'a = 100' // block scope unsafe -> global scope
     let b = 200 // block scope safe // -> local scope
     const c = 300 // block scope safe // -> local scope
@@ -20,3 +21,21 @@ function addOne(num) {return num+20}
 
 const addTwo = function(num) {return num+10}
 console.log(addTwo(2));
+
+var myname = "demi"
+function testVar() {
+    // function scope
+    var myname = "John"
+    console.log(myname);
+}
+
+// due to function scope name is not available here
+console.log(myname);
+
+let test;
+try {
+    abcd(test);
+} catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+}
